52ea7911dbfa901089669f5e617713666ff92899
ZhuangSiYuan <zsy143@qq.com>
2019-07-15 15:17:11 +0800

    添加项目文件。

---
+++ Quartz.NET/HelloJob.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace Quartz.NET
+{
+    public class HelloJob : IJob
+    {
+        Task IJob.Execute(IJobExecutionContext context)
+        {
+            return Task.Run(()=> { Console.WriteLine("作业执行!" + DateTime.Now.ToString()); });
+        }
+    }
+}
---
+++ Quartz.NET/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz.Impl;
+
+namespace Quartz.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggerHelper.Info("服务启动:"+DateTime.Now.ToString());
+
+            RunProgram().GetAwaiter().GetResult();
+
+            Console.WriteLine("输入任意字符关闭。。。");
+            Console.ReadKey();
+        }
+
+        private static async Task RunProgram()
+        {
+            try
+            {
+                NameValueCollection props = new NameValueCollection
+                {
+                    { "quartz.serializer.type", "binary" }
+                };
+
+                // 创建作业调度器
+                StdSchedulerFactory factory = new StdSchedulerFactory(props);
+                IScheduler scheduler = await factory.GetScheduler();
+
+                // 启动调度器
+                await scheduler.Start();
+
+                // 创建作业
+                IJobDetail job = JobBuilder.Create<HelloJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity("trigger1", "group1")
+                    .StartNow()//马上执行
+                    //.StartAt(DateTime.Parse("04:00"))//计划某一时间执行
+                    .WithSimpleSchedule(x => x
+                        .WithIntervalInSeconds(5)//执行周期
+                        .RepeatForever()//重复执行无限制次数
+                        //.WithRepeatCount(100)//重复执行100次
+                        )
+                    .Build();
+
+                // 加入到作业调度器中
+                await scheduler.ScheduleJob(job, trigger);
+            }
+            catch (SchedulerException se)
+            {
+                LoggerHelper.Error("任务启动异常", se);
+                await Console.Error.WriteLineAsync(se.ToString());
+            }
+        }
+    }
+}
